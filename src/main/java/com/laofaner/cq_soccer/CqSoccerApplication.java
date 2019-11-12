3cc610ee38836a99ded3bdc17388b70fe29d8104
fanyuzhuo <fanyuzhuo@dooffe.com>
2019-11-12 15:04:14 +0800

    init...

---
+++ src/main/java/com/laofaner/cq_soccer/CqSoccerApplication.java
@@ -0,0 +1,13 @@
+package com.laofaner.cq_soccer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqSoccerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqSoccerApplication.class, args);
+    }
+
+}
